c237c1b8699f478588d1e25d1ff9cba42f7db270
AndrewKuzkin <akuzkin@mail.ru>
2023-10-17 16:21:19 +0300

    Исправил Lab2 в JS

--- JS/Lab2/Prime.js
+++ JS/Lab2/Prime.js
@@ -1,12 +1,27 @@
 // Считываем параметр из командной строки
-const N =  process.argv[2];
+const NN =  process.argv[2];
 
-// Выведем его, чтобы убедиться, что все нормально
-console.log("Введенное число: ", N);
+// Сделаем хотя бы минимальную верификацию
+if (NN == undefined) {
+	console.log(`Запустите программку еще раз, добавив через командную строку в качестве параметра любое натуральное число.`);
+	return
+}                                                                                             ;
+const N = parseInt(NN);
+if  (Number.isInteger(N) && (N < 0)){
+  console.log(`Вы ввели отрицательное число!`, N);  
+  return
+};
+if  (!Number.isInteger(N)){
+  console.log(`Введенные данные не удалось преобразовать к целому числу!`, N);
+  return
+};
+
+// Выведем число, с которым будем работать
+console.log("Будем считать, что вы имели в виду: ", N);
 
 // Является ли число простым
 function isPrime(n) { 
-	if (n==1) return false;
+	if (n<=1) return false;
     for (let i = 2; i < n; i++) {
       if (n % i === 0) return false;
     }
@@ -23,7 +38,7 @@ function isPrime(n) {
     return N
   }
  
-  if (isPrime(N)) {
-    console.log(N, "- простое число")
-  }
-  else  console.log(N, "- не является простым числом");
+  if (isPrime(N)) console.log(N, "- простое число")
+else  console.log(N, "- не является простым числом");
+
+console.log(`Количество простых чисел, меньших`, N, numberofPrimes(N));
