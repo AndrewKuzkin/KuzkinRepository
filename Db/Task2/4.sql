db28d564e81f8f5b3c63585c2282682befdd3ea7
AndrewKuzkin <akuzkin@mail.ru>
2024-03-21 12:05:07 +0300

    Добавил сообщений

--- Db/Task2/4.sql
+++ Db/Task2/4.sql
@@ -1,4 +1,6 @@
 INSERT INTO messages (userid, chatid, msgtext) 
 VALUES 
 (1, 1, 'Hi Alexander!  Do you have a dog ?'),
-(2, 1, 'I do not have a dog..');
+(1, 1, 'Who is better,  mans or  dogs?'),
+(2, 1, 'I do not have a dog.'),
+(2, 1, 'And the pigs were better.');
--- Db/Task2/5.sql
+++ Db/Task2/5.sql
@@ -1,7 +1,5 @@
-SELECT u.id AS userId, u.firstname AS userName, u.secondname AS userLastname,
-       c.chatid AS chatId, c.chatname AS chatName,
-       MAX(m.messageid) AS lastMessageId, m.msgtext AS lastMessageText
+SELECT  u.firstname, u.secondname ,MAX(m.messageid) AS lastMessageId, m.msgtext 
 FROM users u
 LEFT JOIN messages m ON u.id = m.userid
 LEFT JOIN chats c ON m.chatid = c.chatid
-GROUP BY u.id, u.firstname, u.secondname, c.chatid, c.chatname, m.msgtext;
+	GROUP BY u.id, u.firstname, u.secondname, c.chatid, c.chatname, m.msgtext;
