044fb96a4a6677e4edde6885505dea340af00b15
AndrewKuzkin <akuzkin@mail.ru>
2024-04-05 16:14:23 +0300

    JS2: удалил лишнее

--- JS2/web-solfeggio/parsemelody.js
+++
@@ -1,7 +0,0 @@
-function parseMelody(melody) {
-    return melody.split(', ').map((item, index) => index % 2 === 0 ? item : parseInt(item));
-}
-
-const melodyStr = 'E4, 1000, C4, 750, D4, 250, E4, 1000, A4, 750, G#4, 250, E4, 3000';
-const melody = parseMelody(melodyStr);
-console.log(melody);
