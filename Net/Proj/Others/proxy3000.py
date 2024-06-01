77608e4145b7c51442d42b433423e338e27edc79
AndrewKuzkin <akuzkin@mail.ru>
2024-06-01 16:07:06 +0300

    clear

--- Net/Proj/Others/proxy3000.py
+++
@@ -1,8 +0,0 @@
-from mitmproxy import http
-
-def response(flow: http.HTTPFlow) -> None:
-    if flow.request.pretty_url.startswith("http://192.168.1.75:3000"):
-        if "sql" in flow.request.text:
-            print("Potential SQL injection detected in request:")
-            print(flow.request.text)
-
